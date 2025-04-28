fcf4b91b9c50575c56ec1dae584cbfa6b1b49b8f
Ismail M <ismail.mhmmd@proton.me>
2025-04-28 12:17:39 +0300

    feat: initialize Next.js project with Chakra UI and ESLint configuration
    - Added package.json with dependencies and scripts for development and linting.
    - Created custom _app.js to wrap the application with ChakraProvider and include a Header component.
    - Implemented custom _document.js for setting up HTML structure and meta tags.
    - Developed index.js as the main landing page with sections for About Me, Work Experience, and Side Projects.
    - Added favicon and profile image to the public directory.
    - Created global CSS styles for consistent theming and typography.
    - Implemented color utility functions to generate color palettes.
    - Defined a custom theme using Chakra UI's extendTheme function for consistent styling across components.

---
+++ components/Experience.js
@@ -0,0 +1,47 @@
+import { Text, Heading, GridItem } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { LuExternalLink } from 'react-icons/lu';
+import { Grid } from './Grid';
+
+export function Experience({
+  side, title, desc = '', stack = '', href = '', ...props
+}) {
+  return (
+    <Grid templateColumns="repeat(4, 1fr)" mb={10} {...props}>
+      <GridItem colSpan={{ base: 4, sm: 1 }}>
+        <Text color="white" opacity={0.5}>
+          {side}
+        </Text>
+      </GridItem>
+      <GridItem colSpan={{ base: 4, sm: 3 }}>
+        <Heading
+          as={href ? 'a' : 'h3'}
+          href={href}
+          target="_blank"
+          size="md"
+          display="flex"
+          alignItems="center"
+        >
+          {title}
+          {href && (
+          <LuExternalLink style={{ marginLeft: '0.5rem' }} />
+          )}
+        </Heading>
+        {desc && <Text my={2}>{desc}</Text>}
+        {stack && (
+        <Text color="white" opacity={0.5}>
+          {stack}
+        </Text>
+        )}
+      </GridItem>
+    </Grid>
+  );
+}
+
+Experience.propTypes = {
+  side: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string,
+  stack: PropTypes.string,
+  href: PropTypes.string,
+};
