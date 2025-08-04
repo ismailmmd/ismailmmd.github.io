e9e3f02a70d04061dfb9f7624994897bb96aeef2
Ismail M <ismail.mhmmd@proton.me>
2025-08-04 12:37:23 +0300

    chore: update dependencies and refactor theme configuration
    - Upgraded @chakra-ui/react to version 3.24.0 and next to version 15.4.5.
    - Updated react and react-dom to version 19.1.1.
    - Refactored theme setup in utils/theme.js to use createSystem and defaultConfig.
    - Changed theme import in _app.jsx to reflect new theme structure.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
-import theme from '../utils/theme';
+import system from '../utils/theme';
 import Header from '../components/Header';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider value={system}>
       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
