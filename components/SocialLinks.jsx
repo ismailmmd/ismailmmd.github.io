7719a607cf20441802b200ebd9371c0165d555c4
Ismail M <ismail.mhmmd@proton.me>
2025-08-04 12:49:11 +0300

    refactor: replace Popover with HoverCard for GitHub and StackOverflow links

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import {
-  Link, Flex, GridItem, Grid, Popover, Portal, Image, Box,
+  Link, Flex, GridItem, Grid, HoverCard, Portal, Image, Box,
 } from '@chakra-ui/react';
 import { FaStackOverflow, FaLinkedin, FaGithub } from 'react-icons/fa';
 import React from 'react';
@@ -11,75 +11,62 @@ export default function SocialLinks() {
   return (
     <Grid templateColumns="repeat(5, 1fr)" mb={10} alignItems="flex-start">
       <GridItem>
-        <Link href="https://www.linkedin.com/in/ismail-mohmmd/">
+        <Link href="https://www.linkedin.com/in/ismail-mohmmd/" color="whiteAlpha.900">
           <Flex align="center" gap="2">
-            {' '}
             <FaLinkedin />
-            {' '}
             LinkedIn
-            {' '}
           </Flex>
         </Link>
       </GridItem>
       <GridItem>
-        <Popover.Root openDelay={200} positioning={{ placement: 'bottom' }}>
-          <Popover.Trigger asChild>
-            <Link href="https://github.com/ismailmmd">
+        <HoverCard.Root openDelay={200} closeDelay={100}>
+          <HoverCard.Trigger asChild>
+            <Link href="https://github.com/ismailmmd" color="whiteAlpha.900">
               <Flex align="center" gap="2">
-                {' '}
                 <FaGithub />
-                {' '}
                 Github
-                {' '}
               </Flex>
             </Link>
-          </Popover.Trigger>
+          </HoverCard.Trigger>
           <Portal>
-            <Popover.Positioner>
-              <Popover.Content width="300px" boxShadow="lg" borderRadius="md" bg={bgColor}>
-                <Popover.Body>
-                  <Box p={2}>
-                    <Image
-                      src="https://github-readme-streak-stats.herokuapp.com?user=ismailmmd&exclude_days=Sun%2CSat"
-                      alt="GitHub Streak"
-                      width="100%"
-                    />
-                  </Box>
-                </Popover.Body>
-              </Popover.Content>
-            </Popover.Positioner>
+            <HoverCard.Positioner>
+              <HoverCard.Content width="300px" boxShadow="lg" borderRadius="md" bg={bgColor}>
+                <Box p={2}>
+                  <Image
+                    src="https://github-readme-streak-stats.herokuapp.com?user=ismailmmd&exclude_days=Sun%2CSat"
+                    alt="GitHub Streak"
+                    width="100%"
+                  />
+                </Box>
+              </HoverCard.Content>
+            </HoverCard.Positioner>
           </Portal>
-        </Popover.Root>
+        </HoverCard.Root>
       </GridItem>
       <GridItem>
-        <Popover.Root openDelay={200} positioning={{ placement: 'bottom' }}>
-          <Popover.Trigger asChild>
-            <Link href="https://stackoverflow.com/users/7962589/ismail">
+        <HoverCard.Root openDelay={200} closeDelay={100}>
+          <HoverCard.Trigger asChild>
+            <Link href="https://stackoverflow.com/users/7962589/ismail" color="whiteAlpha.900">
               <Flex align="center" gap="2">
-                {' '}
                 <FaStackOverflow />
-                {' '}
                 StackOverflow
-                {' '}
               </Flex>
             </Link>
-          </Popover.Trigger>
+          </HoverCard.Trigger>
           <Portal>
-            <Popover.Positioner>
-              <Popover.Content width="300px" boxShadow="lg" borderRadius="md" bg={bgColor}>
-                <Popover.Body>
-                  <Box p={2}>
-                    <Image
-                      src="https://stackoverflow-card.vercel.app/?userID=7962589&theme=stackoverflow-light"
-                      alt="StackOverflow Stats"
-                      width="100%"
-                    />
-                  </Box>
-                </Popover.Body>
-              </Popover.Content>
-            </Popover.Positioner>
+            <HoverCard.Positioner>
+              <HoverCard.Content width="300px" boxShadow="lg" borderRadius="md" bg={bgColor}>
+                <Box p={2}>
+                  <Image
+                    src="https://stackoverflow-card.vercel.app/?userID=7962589&theme=stackoverflow-light"
+                    alt="StackOverflow Stats"
+                    width="100%"
+                  />
+                </Box>
+              </HoverCard.Content>
+            </HoverCard.Positioner>
           </Portal>
-        </Popover.Root>
+        </HoverCard.Root>
       </GridItem>
     </Grid>
   );
